97b08e9edbc69c99f34533c746de72dd98d97938
prodigysml <sajeeb.lohani@bugcrowd.com>
2023-05-07 19:52:45 +1000

    updated tests

--- tests/test_rate_limit_queue.py
+++ tests/test_rate_limit_queue.py
@@ -15,3 +15,20 @@ def test_time_period():
     assert r.time_period == 610
     r = RateLimitQueue(10, secs=10, hours=10)
     assert r.time_period == 36010
+
+
+def test_put_items():
+    r = RateLimitQueue(3, secs=1)
+    r.put("test1")
+    r.put("test2")
+    r.put("test3")
+
+    for i in range(1, 4):
+        assert r.get() == f"test{i}"
+
+    range_list = list(range(10))
+
+    r.put_list(range_list)
+
+    for i in range(3):
+        assert r.get() == i
